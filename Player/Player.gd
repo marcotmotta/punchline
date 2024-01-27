b254e3c90147af997529933a04eb5d3f934381b6
Gustavo Zille <guzillaine@gmail.com>
2024-01-27 19:55:23 -0300

    player attack combo logic

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,11 +4,15 @@ const WALK_SPEED_MOD = 10
 const RUN_SPEED_MOD = 5
 const RUN_DURATION_TIME = 1
 
-var is_attacking = false
 var is_running = false
-var is_holding_an_item = false
 
+var is_holding_an_item = false
 var item_held = null
+
+var is_attacking = false
+var attack_combo = 1
+var max_attack_combo = 4
+
 var last_key_pressed = null
 
 # Custom timers.
@@ -55,14 +59,17 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _unhandled_input(event):
-	if Input.is_action_just_pressed("z"):
-		is_attacking = true
-		$AnimationPlayer.play('Combo_1')
-
 	if Input.is_action_just_pressed("x"):
+		if is_attacking:
+			attack_combo = min(attack_combo + 1, max_attack_combo)
+		else:
+			$AnimationPlayer.play('Combo_1')
+			is_attacking = true
+
+	if Input.is_action_just_pressed("z"):
 		$Area3D.monitoring = true
 
-	if Input.is_action_just_released("x"):
+	if Input.is_action_just_released("z"):
 		$Area3D.monitoring = false
 
 	if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
@@ -80,10 +87,34 @@ func _unhandled_input(event):
 			remaining_time_to_press_key = 0
 			remaining_time_to_run = 0
 
+func _reset_attack_combo():
+	is_attacking = false
+	attack_combo = 1
+	$AnimationPlayer.play('Idle')
+
 func _on_animation_player_animation_finished(anim_name):
-	if anim_name == 'Combo_1':
-		is_attacking = false
-		$AnimationPlayer.play('Idle')
+	if anim_name in ['Combo_1', 'Combo_2', 'Combo_3', 'Combo_4']:
+		if attack_combo > 1:
+			match anim_name:
+				'Combo_1':
+					if attack_combo >= 2:
+						$AnimationPlayer.play('Combo_2')
+					else:
+						_reset_attack_combo()
+				'Combo_2':
+					if attack_combo >= 3:
+						$AnimationPlayer.play('Combo_3')
+					else:
+						_reset_attack_combo()
+				'Combo_3':
+					if attack_combo >= 4:
+						$AnimationPlayer.play('Combo_4')
+					else:
+						_reset_attack_combo()
+				_:
+					_reset_attack_combo()
+		else:
+			_reset_attack_combo()
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group('item') and not is_holding_an_item:
