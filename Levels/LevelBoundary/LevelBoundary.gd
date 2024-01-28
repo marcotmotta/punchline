d3c994ea45f169fbf32b71c34abee7233c91ddb7
Gustavo Zille <guzillaine@gmail.com>
2024-01-28 20:03:23 -0300

    trying to fix camera transitions

--- Levels/LevelBoundary/LevelBoundary.gd
+++ Levels/LevelBoundary/LevelBoundary.gd
@@ -1,16 +1,11 @@
 extends Node3D
 
-@export var trigger_stride: float
-@export var boundary_stride: float
 @export var root_scene_node: Node3D
 @export var main_camera: Camera3D
-@export var fixed_camera: Camera3D
-@export var fixed_camera_position: Vector3
 @export var awaiting_enemies_node: Node3D
 
-func _ready():	
-	$Area3D.position.x += trigger_stride
-	$StaticBody3D2.position.x += boundary_stride
+func _ready():
+	pass
 
 func _process(delta):
 	var release_boundary = true
@@ -21,19 +16,17 @@ func _process(delta):
 			break
 
 	if release_boundary:
-		$StaticBody3D2/CollisionShape3D.set_deferred('disabled', true)
+		camera_smooth_change($Camera3D, main_camera)
 
-		camera_smooth_change(fixed_camera, main_camera)
+		$StaticBody3D2/CollisionShape3D.set_deferred('disabled', true)
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group('player'):
+		$Area3D.set_deferred('monitoring', false)
 		$StaticBody3D1/CollisionShape3D.set_deferred('disabled', false)
 		$StaticBody3D2/CollisionShape3D.set_deferred('disabled', false)
 
-		fixed_camera.position = fixed_camera_position
-		camera_smooth_change(main_camera, fixed_camera)
-
-		$Area3D.set_deferred('monitoring', false)
+		camera_smooth_change(main_camera, $Camera3D)
 
 		await get_tree().create_timer(1).timeout 
 
--- Main.gd
+++ Main.gd
@@ -2,27 +2,4 @@ extends Node3D
 
 func _process(delta):
 	#if $Player.position.x >= $Camera3D.position.x:
-	$MainCamera3D.position.x = $Player.position.x
-
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("f1"):
-		camera_smooth_change($MainCamera3D, $FixedCamera3D)
-	if Input.is_action_just_pressed("f2"):
-		camera_smooth_change($FixedCamera3D, $MainCamera3D)
-
-func camera_smooth_change(from, to):
-	var cam = Camera3D.new()
-	cam.projection = Camera3D.PROJECTION_ORTHOGONAL
-	cam.size = from.size
-	cam.position = from.position
-	cam.rotation = from.rotation
-
-	add_child(cam)
-	cam.current = true
-
-	var camera_tween = create_tween()
-	camera_tween.tween_property(cam, "position", to.global_position, 1).set_trans(Tween.TRANS_SINE)
-	camera_tween.tween_callback(camera_tween_callback.bind(to))
-
-func camera_tween_callback(to):
-	to.current = true
+	$Camera3D.position.x = $Player.position.x
